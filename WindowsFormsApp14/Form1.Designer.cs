5a3c7621195aade16b851531901ff6c847e7c574
‪ahmad rahhal‬‏ <ahmad.2001.rahhal@gmail.com>
2023-01-07 16:34:59 +0300

    final

--- WindowsFormsApp14/Form1.Designer.cs
+++ WindowsFormsApp14/Form1.Designer.cs
@@ -48,15 +48,18 @@
             this.checkBox15 = new System.Windows.Forms.CheckBox();
             this.checkBox16 = new System.Windows.Forms.CheckBox();
             this.checkBox17 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(31, 368);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(219, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "house drawn";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -173,7 +176,6 @@
             this.checkBox10.Text = "12";
             this.checkBox10.UseVisualStyleBackColor = true;
             this.checkBox10.Visible = false;
-            this.checkBox10.CheckedChanged += new System.EventHandler(this.checkBox10_CheckedChanged);
             // 
             // checkBox11
             // 
@@ -198,7 +200,6 @@
             this.checkBox12.Text = "9";
             this.checkBox12.UseVisualStyleBackColor = true;
             this.checkBox12.Visible = false;
-            this.checkBox12.CheckedChanged += new System.EventHandler(this.checkBox10_CheckedChanged);
             // 
             // checkBox13
             // 
@@ -255,11 +256,40 @@
             this.checkBox17.UseVisualStyleBackColor = true;
             this.checkBox17.Visible = false;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(31, 399);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "sarqle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(215, 428);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(150, 397);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 8;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.checkBox9);
             this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.checkBox7);
@@ -307,6 +337,9 @@
         private System.Windows.Forms.CheckBox checkBox15;
         private System.Windows.Forms.CheckBox checkBox16;
         private System.Windows.Forms.CheckBox checkBox17;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- WindowsFormsApp14/Form1.cs
+++ WindowsFormsApp14/Form1.cs
@@ -29,9 +29,13 @@ namespace WindowsFormsApp14
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            this.CreateGraphics().Clear(Color.White);
             timer1.Enabled = true;
-            //i = 0;
+            z = 0;
+            i = 0;
+            y = 0;
+          
+
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -63,48 +67,87 @@ namespace WindowsFormsApp14
             Point point13 = new Point(406, 199);
             Point point14 = new Point(336, 199);
             Point point15 = new Point(336, 254);
-            Point point16 = new Point(406, 254);
+            Point point16 = new Point(406, 254); 
             Point[] windo = { point13, point14, point15, point16, point13 };
 
+            
 
+            //this if drawing the house
             if (i < points.Length - 1)
             {
                 p1 = points[i];
                 p2 = points[i+1];
                 this.CreateGraphics().DrawLine(Pens.Blue, p1, p2);
                 i++;
-               
             }
+
+            //this if drawing the Circle
             if (i == points.Length - 1)
             {
-                this.CreateGraphics().DrawEllipse(Pens.Blue, 509, 113, 30,30);
+                this.CreateGraphics().DrawEllipse(Pens.Blue, 509, 113, 30, 30);
                 i++;
             }
+
+            //this if drawing the door
             if (y < door.Length - 1 && i== points.Length)
             {
-
                 p1 = door[y];
                 p2 = door[y + 1];
                 this.CreateGraphics().DrawLine(Pens.Blue, p1, p2);
                 y++;
-                
             }
 
+            //this if drawing the windo
             if (z < windo.Length - 1 && y == door.Length - 1)
             {
-
                 p1 = windo[z];
                 p2 = windo[z + 1];
                 this.CreateGraphics().DrawLine(Pens.Blue, p1, p2);
                 z++;
-
             }
+
+
+            //if (z < windo.Length - 1 && z == door.Length - 1)
+            //{
+
+            //    p1 = windo[z];
+            //    p2 = windo[z + 1];
+            //    this.CreateGraphics().DrawEllipse(Pens.Blue, 509, 113, 30, 30);
+            //    this.CreateGraphics().DrawLine(Pens.Blue, p1, p2);
+            //    z++;
+
+            //}
 
         }
 
-        private void checkBox10_CheckedChanged(object sender, EventArgs e)
-        {
+      
 
+       private void button2_Click(object sender, EventArgs e)
+       {
+            timer1.Enabled = false;
+            this.CreateGraphics().Clear(Color.White);
+            Point point17 = new Point(215, 428);
+            Font font = new Font("Arial", 8);
+            SolidBrush drawBrush = new SolidBrush(Color.Red);
+            string str = "*enter the number less than 100";
+            try
+            {
+               
+                int r = int.Parse(textBox1.Text);
+                if(r <= 100)
+                this.CreateGraphics().FillEllipse(Brushes.Green, 509, 113, r, r);
+                else this.CreateGraphics().DrawString(str, font, drawBrush, point17);
+
+            }
+
+            catch
+            {
+                this.CreateGraphics().DrawString(str, font, drawBrush, point17);
+                
+            }
+           
+            
+            
         }
     }
 }
